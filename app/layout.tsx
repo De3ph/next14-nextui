cb2417f266a45af74c19fe83a8f1a9f11e56477d
Hamit Can <62203579+De3ph@users.noreply.github.com>
2023-11-13 18:15:22 +0300

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import { NextUIProvider } from "@nextui-org/react";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <NextUIProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </NextUIProvider>
+  );
+}
